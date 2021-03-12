edc2292c75bb30e697166100824b514e8c1132e1
mbish08 <mdrew8@gmail.com>
2021-03-12 11:38:32 -0800

    commit tree 113c20d2247ba83557c1771020db8de199ba4cb2

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    changeText()
+})
+
+function changeText() {
+    let newText = document.getElementById("text")
+    newText.textContent = 'This is really cool!'
+}
